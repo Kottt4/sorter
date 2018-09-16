cb9545a3e7d15e965bac88187438f63426fbe28c
Kottt4 <vc@tut.by>
2018-09-16 20:29:57 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
-
-  add(element) {
-    // your implementation
-  }
-
+  constructor () {
+      this.name = [];
+      this.comFun = function (a,b){
+        if (a>b) return 1;
+        if (a<b) return -1;
+        };
+                  
+  };
+  
+  add (element){
+      this.name.push(element)
+  };
+  
   at(index) {
-    // your implementation
-  }
-
-  get length() {
-    // your implementation
-  }
-
-  toArray() {
-    // your implementation
-  }
-
+      return this.name[index];
+    };
+    
+  get length () {
+  return this.name.length;
+  };
+  
+  toArray () {
+  return this.name
+  };
+  
   sort(indices) {
-    // your implementation
-  }
+  var tempArray =[];
+const sortFunction = (left, right) => left - right;
+indices.sort(sortFunction);
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
-}
+  for (var i =0; i<indices.length; i++){
+tempArray[i] = this.name[indices[i]];
+  };
+  //
+  tempArray.sort(this.comFun);
+  //
+  for (var j =0; j<indices.length; j++){
+  this.name[indices[j]] = tempArray[j];
+  };
+  //
+  };
+  
+  setComparator (compareFunction) {
+  this.comFun = compareFunction;
+  };
+   
+  };
 
 module.exports = Sorter;
